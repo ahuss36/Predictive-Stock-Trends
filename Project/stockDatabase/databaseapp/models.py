8c9d7c275a2573dc99e4c88a636c8f9a7813d8bf
drakonheal <osiemons@gmail.com>
2023-10-11 20:56:09 -0500

    Reconfigure Database Fields to Match API Calling
    Renamed and redefined model to fit new sqlite database organization (standardized across api calling and django now).

--- Project/stockDatabase/databaseapp/models.py
+++ Project/stockDatabase/databaseapp/models.py
@@ -3,9 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Stock(models.Model):
-    stock_name = models.CharField(max_length=4, unique = True)
-    stock_predicted = models.IntegerField(default=0)
+    stock_ticker = models.TextField(default='')
+    stock_date = models.IntegerField(default=0)
+    stock_close_price = models.FloatField(default=0.0)
+    stock_prediction = models.BooleanField(default=False)
 
     def __str__(self):
-        return (self.stock_name + " $" + str(self.stock_predicted))
+        return (self.stock_ticker + " $" + str(self.stock_close_price))
 
