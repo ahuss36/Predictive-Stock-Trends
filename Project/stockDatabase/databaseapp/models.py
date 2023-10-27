a8e4ad5060780bc768ee24d2e61ec0f2082370ef
drakonheal <osiemons@gmail.com>
2023-10-27 17:08:34 -0500

    Fix date to TextField
    Instead of IntegerField

--- Project/stockDatabase/databaseapp/models.py
+++ Project/stockDatabase/databaseapp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Stock(models.Model):
     stock_ticker = models.TextField(default='')
-    stock_date = models.IntegerField(default=0)
+    stock_date = models.TextField(default=0)
     stock_close_price = models.FloatField(default=0.0)
     stock_prediction = models.BooleanField(default=False)
 
