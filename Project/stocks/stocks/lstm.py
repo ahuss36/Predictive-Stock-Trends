cdb583122c5838f0ebdf0a3d49a5e5458623cd44
Drew Oberweis <drew@oberweis.dev>
2023-11-22 21:27:47 -0600

    Auto stash before merge of "41-final-ml-integration" and "origin/41-final-ml-integration"

--- Project/stocks/stocks/lstm.py
+++ Project/stocks/stocks/lstm.py
@@ -1,6 +1,39 @@
+from . import models
+import pandas as pd
+import tensorflow as tf
+import keras
+
 def trainModel(data, filename):
     return True
 
+def loadModel(filename):
+    try:
+        # load model from file
+        model = keras.models.load_model(filename)
+
 def predict(data, daysOut=3):
+
     if (daysOut > 7):
         daysOut = 7
+
+    def addPredictionValues(values): # save a dataframe of values to the database as predictions
+
+        """
+        Takes a dataframe of this structure:
+        ticker | close | date
+        """
+
+        if (type(values) != pd.DataFrame): # This needs a dataframe for the data
+            return False
+        
+        for i in range(0, daysOut):
+            dbModel = models.Stock()
+
+            dbModel.ticker = values.iloc[i].ticker # I don't know what iloc is
+            dbModel.close = values.iloc[i].close
+            dbModel.date = values.iloc[i].date
+            dbModel.prediction = True # This HAS to be true here
+
+            dbModel.save()
+
+        return True
