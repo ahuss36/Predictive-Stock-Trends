ce4e692ac1b5ddda96a965444155863e1ca498be
Drew Oberweis <drew@oberweis.dev>
2023-11-22 21:38:11 -0600

    delete old backend folder

--- Project/backend/config.py
+++
@@ -1,8 +0,0 @@
-class database:
-    table_name = 'databaseapp_stock'
-    
-    prediction_column_name = "stock_prediction"
-    ticker_column_name = "stock_ticker"
-    price_column_name = "stock_close_price"
-    date_column_name = "stock_date"
-    id_column_name = "id"
