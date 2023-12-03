b31484c967e43dca7510a5e63ea88eacc35e57ab
Drew Oberweis <drew@oberweis.dev>
2023-12-03 13:11:18 -0600

    fix run.bat and update run.md

--- run.bat
+++ run.bat
@@ -1,6 +1,21 @@
 @echo off
 
+set "venv_path=.venv" REM Set the desired virtual environment path/name
+
+if not exist %venv_path% (
+    echo Creating virtual environment...
+    python3.11 -m venv %venv_path%
+    echo Virtual environment created at %venv_path%
+) else (
+    echo Virtual environment already exists at %venv_path%
+)
+
+
 .\.venv\Scripts\python.exe -m pip install -r requirements.txt
+
+.\.venv\Scripts\python.exe .\Project\stocks\manage.py makemigrations stocks 
+.\.venv\Scripts\python.exe .\Project\stocks\manage.py migrate
+
 .\.venv\Scripts\python.exe .\Project\stocks\manage.py runserver
 
 pause
--- run.md
+++ run.md
@@ -1,2 +1,5 @@
-1. Set up a virtual environment, no need to install anything from there.
-2. Run "run.bat", it handles all requirements and runs the server.
+1. Install Python 3.11
+It is important to use this version, as the package dm-tree has not updated to support python 3.12
+
+2. Run "run.bat"
+It sets up a virtual environment if one doesn't already exist, installs all required packages, and then runs the server
